9a0ce35b32821a73f0fa9ecb73f0d30921d44ae0
mohamedeld <mohamed.azoz20010@gmail.com>
2025-04-06 21:39:18 +0200

    add pug view engine template

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const path = require("path");
+const { requireLogin } = require("./middleware");
+const loginRoute = require("./routes/loginRoute");
 require("dotenv").config();
 
 const app = express();
+
+app.use(express.json());
+app.set("view engine","pug");
+
+app.set("views","./views");
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.use("/login",loginRoute);
+app.get("/",requireLogin,(req,res)=>{
+  const payload = {
+    pageTitle:"Home Page",
+  }
+  res.status(200).render("home",payload);
+})
 
 const PORT = process.env.PORT || 8080;
 
---
+++ routes/loginRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+app.set("view engine","pug");
+app.set("views","./views");
+router.route("/").get((req,res)=>{
+    res.status(200).render("login")
+})
+
+module.exports = router;
